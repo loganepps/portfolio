dc5491bc4a05816b0ddea2719c9c242de7a2cfe1
Logan Epps <logepps@gmail.com>
2021-08-31 12:46:36 -0500

    removed some js and blur filter on img

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
-// const toggleStory = document.querySelector('#more-toggle');
-// const story = document.querySelector('#more-about');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -12,11 +10,3 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
-
-// toggleStory.addEventListener('click', () => {
-//     if (story.style.display !== 'none') {
-//         story.style.display = 'none';
-//     } else {
-//         story.style.display = 'block';
-//     }
-// })
